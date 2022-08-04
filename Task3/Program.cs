c361186d647e1795496c6b4ff63e2046c3aaa29b
lavr174 <lavr174@icloud.com>
2022-08-04 21:37:08 +0300

    task3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Дни недели (1-7)
+Console.Clear();
+
+Console.WriteLine("Введите день недели ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 1) Console.WriteLine("Понедельник");
+if (number == 2) Console.WriteLine("Вторник");
+if (number == 3) Console.WriteLine("Среда");
+if (number == 4) Console.WriteLine("Четверг");
+if (number == 5) Console.WriteLine("Пятница");
+if (number == 6) Console.WriteLine("Суббота");
+if (number == 7) Console.WriteLine("Воскресенье");
+else Console.WriteLine("Введите число от 1 до 7");
