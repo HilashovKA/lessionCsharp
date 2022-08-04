f149ebd234b9fcc7408edcb20346061bbe78ebb2
lavr174 <lavr174@icloud.com>
2022-08-04 23:41:08 +0300

    task4

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Clear();
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат.
+
+Console.Clear();
 System.Console.Write("Введите число ");
 int number = Convert.ToInt32(Console.ReadLine());
 int result = number*number;
---
+++ task4/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от - N до N.
+Console.Clear();
+Console.WriteLine("Введите любое целое число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int count = number;
+Console.Write(number);
+while(count > -number)
+{
+    number = number - 1;
+    Console.Write(number) ;
+}
