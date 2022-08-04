dbce46743485c1f5432bbbad033055af4ae8058a
lavr174 <lavr174@icloud.com>
2022-08-04 23:51:28 +0300

    task5

---
+++ task5/Program.cs
@@ -0,0 +1,6 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+Console.WriteLine("Введите трёхзначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int digit = number % 10;
+Console.WriteLine($"Последняя цифра {number} равна {digit}");
